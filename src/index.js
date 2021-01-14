529d5c12d84688b5050f79cd363fac0f4e85417b
Paul Realpe <paulmrg461@gmail.com>
2021-01-14 09:36:39 -0500

    14. Reducers

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './components/App';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import reducers from './reducers';
 
 const store = createStore(
-  {}, //reducers
+  reducers, //reducers
   {} //Init state
 );
  
 ReactDOM.render(
-  <App />,
+  <Provider store = { store }>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
